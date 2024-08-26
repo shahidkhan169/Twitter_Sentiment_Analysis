31d55196fb4056f786777a0a9cb593bd96b92e87
shahidkhan169 <shahid1692004@gmail.com>
2024-08-26 20:33:42 +0530

    first commit

--- backend/build.sh
+++ backend/build.sh
@@ -5,8 +5,9 @@ apt-get update
 apt-get install -y openjdk-11-jdk
 
 # Set JAVA_HOME environment variable
-export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
-export PATH=$JAVA_HOME/bin:$PATH
+set JAVA_HOME=C:\Program Files\Java\jdk-21
+set PATH=%JAVA_HOME%\bin;%PATH%
+
 
 # Install Python dependencies
 pip install -r requirements.txt
